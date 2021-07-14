f57e022cbd02e1e4ef306183b85edb17af38eaed
stephen <sflores813@gmail.com>
2021-07-13 23:57:07 -0500

    working on the log in future, linked with firebase

---
+++ src/components/Signup.js
@@ -0,0 +1,52 @@
+import React, { useRef } from 'react'
+import { Form, Button, Card } from 'react-bootstrap'
+import { useAuth } from '../contexts/AuthContext' 
+
+export default function Signup() {
+    const emailRef = useRef() 
+    const passwordRef = useRef()
+    const passwordConfirmRef = useRef()
+    const { signup } = useAuth()
+
+    function handleSubmit(e) {
+        e.prevent()
+
+        signup(emailRef.current.value, passwordRef.current.value)
+    }
+
+
+    return (
+
+        <>
+        <Card>
+            <Card.Body>
+                <h2 className="text-center mb-4">Sign Up</h2>
+                <Form>
+                    <Form.Group id ="email">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="email" ref={emailRef} requird />
+                    </Form.Group>
+                    <Form.Group id ="password">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" ref={passwordRef} requird />
+                    </Form.Group>
+                    <Form.Group id ="passwordConfirm">
+                        <Form.Label>Password Confirm</Form.Label>
+                        <Form.Control type="passwordConfirm" ref={passwordConfirmRef} requird />
+                    </Form.Group>
+                    <Button className="w-100" type="submit">Sign Up</Button>
+                </Form>
+            </Card.Body>
+        </Card>
+        <div className="w-100 text-center mt-2">
+            Already have an account? Log in 
+        </div>
+        </>
+        )
+    }
+
+                
+
+
+
+    
