d126b0b61f32123c3e4408cb501f1bdac47f8b3d
stephen <sflores813@gmail.com>
2021-07-14 00:35:01 -0500

    fix small bugs/ setting up signup functionallity

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,5 +1,5 @@
-import React, { useRef } from 'react'
-import { Form, Button, Card } from 'react-bootstrap'
+import React, { useRef, useState } from 'react'
+import { Form, Button, Card, Alert } from 'react-bootstrap'
 import { useAuth } from '../contexts/AuthContext' 
 
 export default function Signup() {
@@ -7,11 +7,26 @@ export default function Signup() {
     const passwordRef = useRef()
     const passwordConfirmRef = useRef()
     const { signup } = useAuth()
+    const [error, setError] = useState("")
+    const [loading, setLoading] = useState(false)
+    // const history = useHistory
 
-    function handleSubmit(e) {
+    async function handleSubmit(e) {
         e.prevent()
+        if (passwordRef.current.value !== passwordConfirmRef.current.value){
+            return setError('Passwords do not match')
+        }
 
-        signup(emailRef.current.value, passwordRef.current.value)
+        try {
+            setError("")
+            setLoading(true)
+            await signup(emailRef.current.value, passwordRef.current.value)
+        } catch {
+            setError("failed to create an account")
+        }
+
+        setLoading(false)
+        
     }
 
 
@@ -21,25 +36,28 @@ export default function Signup() {
         <Card>
             <Card.Body>
                 <h2 className="text-center mb-4">Sign Up</h2>
-                <Form>
+                {error && <Alert variant="danger">{error}</Alert>}
+                <Form onSubmit={handleSubmit}>
                     <Form.Group id ="email">
                         <Form.Label>Email</Form.Label>
-                        <Form.Control type="email" ref={emailRef} requird />
+                        <Form.Control type="email" ref={emailRef} required />
                     </Form.Group>
                     <Form.Group id ="password">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" ref={passwordRef} requird />
+                        <Form.Control type="password" ref={passwordRef} required />
                     </Form.Group>
-                    <Form.Group id ="passwordConfirm">
+                    <Form.Group id ="password-confirm">
                         <Form.Label>Password Confirm</Form.Label>
-                        <Form.Control type="passwordConfirm" ref={passwordConfirmRef} requird />
+                        <Form.Control type="password" ref={passwordConfirmRef} required />
                     </Form.Group>
-                    <Button className="w-100" type="submit">Sign Up</Button>
+                    <Button disabled={loading} className="w-100" type="submit">
+                        Sign Up
+                        </Button>
                 </Form>
             </Card.Body>
         </Card>
         <div className="w-100 text-center mt-2">
-            Already have an account? Log in 
+            Already have an account? 
         </div>
         </>
         )
