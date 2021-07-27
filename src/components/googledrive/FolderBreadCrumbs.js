99f2a3fbc24d32d21481b667262951e14b596976
stephen <sflores813@gmail.com>
2021-07-27 12:56:44 -0500

    adding breadcrumb function

--- src/components/googledrive/FolderBreadCrumbs.js
+++ src/components/googledrive/FolderBreadCrumbs.js
@@ -1,12 +1,33 @@
 import React from 'react'
 import { Breadcrumb } from 'react-bootstrap'
+import { ROOT_FOLDER } from '../../hooks/useFolder'
+import { Link } from 'react-router-dom'
+
 
 export default function FolderBreadCrumbs({ currentFolder }) {
+    let path = currentFolder === ROOT_FOLDER ? [] : [ROOT_FOLDER]
+    if (currentFolder) path = [...path, ...currentFolder.path]
     return (
         // loop between all folders that are before our current folder, and render out breadcrumb
         <Breadcrumb className="flex-grow-1"  listProps={{ className:"bg-white pl-0 m-0"}}>
+            {path.map((folder, index) => (
+                <Breadcrumb.Item
+                key={folder.id}
+                linkAs={Link} 
+                className="text-truncate d-inline-block"  
+                style={{ maxWidth: "150px" }} 
+                >
+                
+                {folder.name}
+                </Breadcrumb.Item>
+            ))}
+                
+                
         {currentFolder && (
-            <Breadcrumb.Item className="text-truncate d-inline-block"  style={{ maxWidth: "200px" }} active>
+            <Breadcrumb.Item 
+            className="text-truncate d-inline-block"  
+            style={{ maxWidth: "200px" }} 
+            active>
             {currentFolder.name}
             </Breadcrumb.Item>
         )}
